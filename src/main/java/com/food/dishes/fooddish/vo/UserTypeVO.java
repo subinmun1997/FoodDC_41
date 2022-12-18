b5db778df233da642d47ac5c562c0378cf03ce8e
subinmun1997 <qzxy812@gmail.com>
2022-12-18 17:57:35 +0900

    feat: Create userVO

---
+++ src/main/java/com/food/dishes/fooddish/vo/UserTypeVO.java
@@ -0,0 +1,32 @@
+package com.food.dishes.fooddish.vo;
+
+public class UserTypeVO {
+
+    private String id;
+    private String name;
+
+    public UserTypeVO() {}
+
+    public UserTypeVO(String id, String name) {
+        super();
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
