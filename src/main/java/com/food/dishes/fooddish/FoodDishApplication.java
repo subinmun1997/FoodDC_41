0c811e698b87b45e8c80c7ca49eb6a8f9f769666
subinmun1997 <qzxy812@gmail.com>
2022-12-18 16:41:30 +0900

    Init project

---
+++ src/main/java/com/food/dishes/fooddish/FoodDishApplication.java
@@ -0,0 +1,13 @@
+package com.food.dishes.fooddish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodDishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodDishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/food/dishes/fooddish/FoodDishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food.dishes.fooddish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
