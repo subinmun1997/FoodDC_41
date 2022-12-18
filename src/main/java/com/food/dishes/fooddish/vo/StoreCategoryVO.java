4644f6dd635af91d7810c5d65451a89b890f82aa
subinmun1997 <qzxy812@gmail.com>
2022-12-18 22:01:25 +0900

    feat: Create All-StoreVO

---
+++ src/main/java/com/food/dishes/fooddish/vo/StoreCategoryVO.java
@@ -0,0 +1,30 @@
+package com.food.dishes.fooddish.vo;
+
+public class StoreCategoryVO {
+
+    private String id;
+    private String name;
+
+    public StoreCategoryVO() {}
+
+    public StoreCategoryVO(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/food/dishes/fooddish/vo/StoreListByUserViewVO.java
@@ -0,0 +1,61 @@
+package com.food.dishes.fooddish.vo;
+
+public class StoreListByUserViewVO {
+
+    private int storeId;
+    private String storeName;
+    private String storePhoto;
+    private int distance;
+    private double reviewScore;
+
+    public StoreListByUserViewVO() {}
+
+    public StoreListByUserViewVO(int storeId, String storeName, String storePhoto, int distance, double reviewScore) {
+        super();
+        this.storeId = storeId;
+        this.storeName = storeName;
+        this.storePhoto = storePhoto;
+        this.distance = distance;
+        this.reviewScore = reviewScore;
+    }
+
+    public int getStoreId() {
+        return storeId;
+    }
+
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getStorePhoto() {
+        return storePhoto;
+    }
+
+    public void setStorePhoto(String storePhoto) {
+        this.storePhoto = storePhoto;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public double getReviewScore() {
+        return reviewScore;
+    }
+
+    public void setReviewScore(double reviewScore) {
+        this.reviewScore = reviewScore;
+    }
+}
