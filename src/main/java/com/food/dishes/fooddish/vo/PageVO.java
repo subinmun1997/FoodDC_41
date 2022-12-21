06c26313d549e22b477b538e03c918359ee3523b
subinmun1997 <qzxy812@gmail.com>
2022-12-21 19:57:49 +0900

    feat: Create PageVO

---
+++ src/main/java/com/food/dishes/fooddish/vo/PageVO.java
@@ -0,0 +1,38 @@
+package com.food.dishes.fooddish.vo;
+
+public class PageVO {
+
+    private String showPageName;
+    private int pageNumber;
+
+    public PageVO() {}
+
+    public PageVO(String showPageName, int pageNumber) {
+        this.showPageName = showPageName;
+        this.pageNumber = pageNumber;
+    }
+
+    public String getShowPageName() {
+        return showPageName;
+    }
+
+    public void setShowPageName(String showPageName) {
+        this.showPageName = showPageName;
+    }
+
+    public int getPageNumber() {
+        return pageNumber;
+    }
+
+    public void setPageNumber(int pageNumber) {
+        this.pageNumber = pageNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PageVO{" +
+                "showPageName='" + showPageName + '\'' +
+                ", pageNumber=" + pageNumber +
+                '}';
+    }
+}
