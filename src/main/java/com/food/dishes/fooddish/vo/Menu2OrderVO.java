33b951b6e0818c3e3885da435193428d503a5897
subinmun1997 <qzxy812@gmail.com>
2022-12-21 20:05:46 +0900

    feat: Create Menu-VO

---
+++ src/main/java/com/food/dishes/fooddish/vo/Menu2OrderVO.java
@@ -0,0 +1,55 @@
+package com.food.dishes.fooddish.vo;
+
+import java.io.Serializable;
+
+public class Menu2OrderVO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private OrderVO order;
+    private StoreVO store;
+    private MenuVO menu;
+    private int amount;
+
+    public Menu2OrderVO() {}
+
+    public Menu2OrderVO(OrderVO order, StoreVO store, MenuVO menu, int amount) {
+        this.order = order;
+        this.store = store;
+        this.menu = menu;
+        this.amount = amount;
+    }
+
+    public OrderVO getOrder() {
+        return order;
+    }
+
+    public void setOrder(OrderVO order) {
+        this.order = order;
+    }
+
+    public StoreVO getStore() {
+        return store;
+    }
+
+    public void setStore(StoreVO store) {
+        this.store = store;
+    }
+
+    public MenuVO getMenu() {
+        return menu;
+    }
+
+    public void setMenu(MenuVO menu) {
+        this.menu = menu;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
+
---
+++ src/main/java/com/food/dishes/fooddish/vo/Menu2OrderViewVO.java
@@ -0,0 +1,50 @@
+package com.food.dishes.fooddish.vo;
+
+public class Menu2OrderViewVO {
+
+    private int orderId;
+    private String storeName;
+    private String menuName;
+    private int amount;
+
+    public Menu2OrderViewVO() {}
+
+    public Menu2OrderViewVO(int orderId, String storeName, String menuName, int amount) {
+        this.orderId = orderId;
+        this.storeName = storeName;
+        this.menuName = menuName;
+        this.amount = amount;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getMenuName() {
+        return menuName;
+    }
+
+    public void setMenuName(String menuName) {
+        this.menuName = menuName;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
