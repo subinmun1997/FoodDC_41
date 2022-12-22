3dec767b548c446c4a3b52c03b97929369591a0d
subinmun1997 <qzxy812@gmail.com>
2022-12-22 14:07:21 +0900

    feat: Create map-Exception

---
+++ src/main/java/com/food/dishes/fooddish/exception/map/MapException.java
@@ -0,0 +1,10 @@
+package com.food.dishes.fooddish.exception.map;
+
+public class MapException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public MapException(String message) {
+        super(message);
+    }
+}
