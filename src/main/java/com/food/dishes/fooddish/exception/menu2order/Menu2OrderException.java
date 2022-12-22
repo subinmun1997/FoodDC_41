938535a9f63e7c9e9b20136b005ee15cab70d893
subinmun1997 <qzxy812@gmail.com>
2022-12-22 15:24:27 +0900

    feat: Create menu2order-Exception

---
+++ src/main/java/com/food/dishes/fooddish/exception/menu2order/Menu2OrderException.java
@@ -0,0 +1,10 @@
+package com.food.dishes.fooddish.exception.menu2order;
+
+public class Menu2OrderException extends RuntimeException{
+
+    private static final long serialVersionUID = 1L;
+
+    public Menu2OrderException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/food/dishes/fooddish/exception/menu2order/Menu2OrderInsertException.java
@@ -0,0 +1,15 @@
+package com.food.dishes.fooddish.exception.menu2order;
+
+public class Menu2OrderInsertException extends Menu2OrderException {
+
+    private static final long serialVersionUID = 1L;
+    private static final String MESSAGE = "주문 목록 등록에 실패했습니다.";
+
+    public Menu2OrderInsertException() {
+        this(MESSAGE);
+    }
+
+    public Menu2OrderInsertException(String message) {
+        super(message);
+    }
+}
